579de6ec37fea11dc14104bb9a53b8ed89391360
Simone Tiraboschi <stirabos@redhat.com>
2025-04-29 10:08:56 +0200

    Introduce additional env variables
    - Introduce additional env variables
    - clean shellcheck warnings
    
    Signed-off-by: Simone Tiraboschi <stirabos@redhat.com>

--- launch-n-sequences.sh
+++ launch-n-sequences.sh
@@ -13,30 +13,45 @@ MAXWRITESECONDS=${MAXWRITESECONDS:-300}
 PVCACCESSMODE=${PVCACCESSMODE:-ReadWriteMany}
 # Number of iteration (write->sleep) for each PVC
 ITERATIONS=${ITERATIONS:-10}
+# Name of the storage class to be used for the test, empty will choose then default one
+STORAGECLASS=${STORAGECLASS:-""}
+# Storage request for test PVCs
+STORAGEREQUEST=${STORAGEREQUEST:-"1Gi"}
+# Number of seconds of delay after the creation of each job
+RAMPUPDELAYSECONDS=${RAMPUPDELAYSECONDS:-"1"}
 
 echo "##########"
 echo "This script will create ${NUMPARALLEL} parallel jobs."
 echo "Each job will create a ${PVCACCESSMODE} PVC and it will iterate ${ITERATIONS} times the creation of a pod to keep the PVC busy followed by another pod that is not going to use it."
 echo "Each \"diskwriter\" pod will keep the PV busy (mounted) for a random number of seconds between ${MINWRITESECONDS} and ${MAXWRITESECONDS}, the following \"sleeper\" pod will simply sleep for ${SLEEPSECONDS} seconds."
-EXPELAPSED=$(( (($MINWRITESECONDS + $MAXWRITESECONDS)/2 + $SLEEPSECONDS) * $ITERATIONS ))
-echo "The test is expected to be executed in around $EXPELAPSED seconds ($(($EXPELAPSED/3600)) hours)."
-echo "The test is expected to perform $(( $NUMPARALLEL * 3600 / ((($MINWRITESECONDS + $MAXWRITESECONDS)/2 + $SLEEPSECONDS) ))) map/unmap cycles/hour."
+EXPELAPSED=$(( ((MINWRITESECONDS + MAXWRITESECONDS)/2 + SLEEPSECONDS) * ITERATIONS ))
+echo "The test is expected to be executed in around $EXPELAPSED seconds ($((EXPELAPSED/3600)) hours)."
+echo "The test is expected to perform $(( NUMPARALLEL * 3600 / ((MINWRITESECONDS + MAXWRITESECONDS)/2 + SLEEPSECONDS) )) map/unmap cycles/hour."
 echo "##########"
 echo
 echo "Deleting leftovers..."
-oc delete job -n ${NAMESPACE} -l=name=pv-sequence-job
-oc wait --for=delete -n ${NAMESPACE} pvc -l=type=pvc-sequence --timeout=180s
+oc delete job -n "${NAMESPACE}" -l=name=pv-sequence-job
+oc wait --for=delete -n "${NAMESPACE}" pvc -l=type=pvc-sequence --timeout=180s
 echo "Creating resources..."
-oc apply -n ${NAMESPACE} -f pod-templates-configmap.yaml
-oc apply -n ${NAMESPACE} -f serviceaccount.yaml
+oc apply -n "${NAMESPACE}" -f pod-templates-configmap.yaml
+oc apply -n "${NAMESPACE}" -f serviceaccount.yaml
 oc apply -f clusterrole.yaml
-oc apply -n ${NAMESPACE} -f rolebinding.yaml
+oc apply -n "${NAMESPACE}" -f rolebinding.yaml
 
 echo "Launching $NUMPARALLEL sequences..."
 
-for instance in $(seq -w 0001 $NUMPARALLEL); do
-  sed "s/{{INSTANCE}}/$instance/g" sequence-job.yaml | sed "s/{{NAMESPACE}}/$NAMESPACE/g" | sed "s/{{SLEEPSECONDS}}/$SLEEPSECONDS/g" | sed "s/{{MINWRITESECONDS}}/$MINWRITESECONDS/g" | sed "s/{{MAXWRITESECONDS}}/$MAXWRITESECONDS/g" | sed "s/{{PVCACCESSMODE}}/$PVCACCESSMODE/g" | sed "s/{{ITERATIONS}}/$ITERATIONS/g" | oc apply -n ${NAMESPACE} -f -
-  sleep 1
+for instance in $(seq -w 0001 "$NUMPARALLEL"); do
+  sed "s/{{INSTANCE}}/$instance/g" sequence-job.yaml | \
+  sed "s/{{NAMESPACE}}/$NAMESPACE/g" | \
+  sed "s/{{SLEEPSECONDS}}/$SLEEPSECONDS/g" | \
+  sed "s/{{MINWRITESECONDS}}/$MINWRITESECONDS/g" | \
+  sed "s/{{MAXWRITESECONDS}}/$MAXWRITESECONDS/g" | \
+  sed "s/{{PVCACCESSMODE}}/$PVCACCESSMODE/g" | \
+  sed "s/{{ITERATIONS}}/$ITERATIONS/g" | \
+  sed "s/{{STORAGECLASS}}/$STORAGECLASS/g" | \
+  sed "s/{{STORAGEREQUEST}}/$STORAGEREQUEST/g" | \
+  oc apply -n "${NAMESPACE}" -f -
+  sleep "${RAMPUPDELAYSECONDS}"
 done
 
 echo
